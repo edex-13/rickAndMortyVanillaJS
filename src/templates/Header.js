fb9aa62948014f57aed8282eda04268d0073c1bd
Edex13 <ederson12f@gmail.com>
2021-03-14 17:57:55 -0500

    travis

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
         <div class="Header-logo">
             <h1>
-                <a href="/">
+                <a href="/rickAndMortyVanillaJS/">
                     [Rick And Morty Api]
                 </a>
             </h1>
